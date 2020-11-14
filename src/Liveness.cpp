f25744c919fd0fa06a24b8c8d4224293205272ec
Saikat Chakraborty <saikatc@cs.columbia.edu>
2020-11-14 13:08:40 -0500

    Skeleton code added

---
+++ src/Liveness.cpp
@@ -0,0 +1,197 @@
+#include "hw5-util.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Pass.h"
+
+#include <algorithm>
+#include <map>
+#include <set>
+#include <vector>
+
+using namespace llvm;
+using namespace std;
+
+struct Liveness : public FunctionPass {
+  static char ID; // Pass identification, replacement for typeid
+
+  Liveness() : FunctionPass(ID) {}
+
+  set<Value *> setUnion(set<Value *> &a, set<Value *> &b) {
+    set<Value *> result;
+    std::set_union(a.begin(), a.end(), b.begin(), b.end(),
+                   std::inserter(result, result.begin()));
+    return result;
+  }
+
+  set<Value *> setIntersection(set<Value *> &a, set<Value *> &b) {
+    set<Value *> result;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(),
+                          std::inserter(result, result.begin()));
+    return result;
+  }
+
+  set<Value *> setDifference(set<Value *> &a, set<Value *> &b) {
+    set<Value *> result;
+    std::set_difference(a.begin(), a.end(), b.begin(), b.end(),
+                        std::inserter(result, result.begin()));
+    return result;
+  }
+
+  map<Instruction *, vector<Instruction *>>
+  extractControlFlowGraph(Function &F) {
+    map<Instruction *, vector<Instruction *>> controlFlowGraph;
+    Instruction *preI = nullptr;
+    for (BasicBlock &BB : F) {
+      const Instruction *TInst = BB.getTerminator();
+      for (Instruction &I : BB) {
+        Instruction *pI = &I;
+        if (preI != nullptr) {
+          vector<Instruction *> successors;
+          if (pI == TInst) {
+            for (int i = 0, nSucc = TInst->getNumSuccessors(); i < nSucc; i++) {
+              BasicBlock *succ = TInst->getSuccessor(i);
+              successors.push_back(&succ->front());
+            }
+            controlFlowGraph.insert({pI, successors});
+          }
+          successors.push_back(pI);
+          controlFlowGraph.insert({preI, successors});
+        }
+        preI = pI;
+      }
+    }
+    return controlFlowGraph;
+  }
+
+  /**
+   * This function populates the initial values of different data flows ingredients.
+   * For every instruction, if it defines anything, `def` will contain that Value
+   * `use` will contain all the values that are used in an instruction.
+   * Initially, both `in` and `out` are populated with empty set.
+   * `phiUse` and `phiIn` are two special cases.
+   * Not all the values are used (and thus `in`) for a `PHINode`.
+   * `PHINode` only uses the values from the basicblock it is coming from.
+   * Thus, for every instruction, `phiUse`, and `phiIN` contains a map
+   * From the incoming `BasicBlock*` to the incoming `Value *` set.
+   * Note that, all the parameters are pointers in this function.
+   * Thus the changes we are making in this function (i.e. we are populating the values)
+   * Will be reflected to the original maps from where it is being ccalled.
+   */
+  void populateInitialValues(
+      vector<Instruction *> &allInstructions,
+      map<Instruction *, set<Value *>> &def,
+      map<Instruction *, set<Value *>> &use,
+      map<Instruction *, set<Value *>> &in,
+      map<Instruction *, set<Value *>> &out,
+      map<PHINode *, map<BasicBlock *, set<Value *>>> &phiUse,
+      map<PHINode *, map<BasicBlock *, set<Value *>>> &phiIn) {
+    for (Instruction *pI : allInstructions) {
+      Value *p = dyn_cast<Value>(pI);
+      set<Value *> s;
+      if (!isa<BranchInst>(pI) && !isa<ReturnInst>(pI)) {
+        s.insert(p);
+      }
+      def[pI] = s;
+      in[pI] = set<Value *>();
+      out[pI] = set<Value *>();
+
+      set<Value *> s2;
+      for (auto opnd = pI->op_begin(); opnd != pI->op_end(); ++opnd) {
+        Value *val = *opnd;
+        if (isa<Instruction>(val) || isa<Argument>(val)) {
+          s2.insert(val);
+        }
+      }
+      use[pI] = s2;
+      // handle PHI NODE
+      if (isa<PHINode>(pI)) {
+        PHINode *phiInstruction = dyn_cast<PHINode>(pI);
+        map<BasicBlock *, set<Value *>> tempUseMap;
+        map<BasicBlock *, set<Value *>> tempInMap;
+        for (unsigned int ind = 0; ind < phiInstruction->getNumIncomingValues();
+             ind++) {
+          set<Value *> tempSet;
+          Value *val = phiInstruction->getIncomingValue(ind);
+          if (isa<Instruction>(val) || isa<Argument>(val)) {
+            BasicBlock *valBlock = phiInstruction->getIncomingBlock(ind);
+            if (tempInMap.find(valBlock) == tempInMap.end()) {
+              tempInMap[valBlock] = set<Value *>();
+            }
+            if (tempUseMap.find(valBlock) == tempUseMap.end()) {
+              tempSet.insert(val);
+              tempUseMap[valBlock] = tempSet;
+            } else {
+              tempUseMap[valBlock].insert(val);
+            }
+          }
+        }
+        phiUse[phiInstruction] = tempUseMap;
+        phiIn[phiInstruction] = tempInMap;
+      }
+    }
+  }
+
+  bool runOnFunction(Function &F) override {
+    DataFlowWriter writer(F);
+    vector<Instruction *> allInstructions = writer.getAllInstructions();
+    /* The Flow graph is represented as adjacency list.
+     * For every Instruction in the graph, controlFlowGraph contains a vector
+     * Of its neighboring Instruction (only outgoing edges). */
+    map<Instruction *, vector<Instruction *>> controlFlowGraph =
+        extractControlFlowGraph(F);
+
+    map<Instruction *, set<Value *>> USE;
+    map<Instruction *, set<Value *>> DEF;
+    map<PHINode *, map<BasicBlock *, set<Value *>>> PHI_USE;
+    map<Instruction *, set<Value *>> IN;
+    map<Instruction *, set<Value *>> OUT;
+    map<PHINode *, map<BasicBlock *, set<Value *>>> PHI_IN;
+    populateInitialValues(allInstructions, DEF, USE, IN, OUT, PHI_USE, PHI_IN);
+
+    /* These two printers show the DEFs and USEs of every instruction. */
+    writer.printDefs(DEF);
+    writer.printUses(USE);
+
+    int iterationCount = 0;
+    bool possibleToUpdate = true;
+    /* NOTE: Please do no change in code in this function upto this point */
+    /* If you change anything, and the output format does not match, your assignment will be penalized.*/
+
+
+    /*
+     * Initially, we assume that dataflow update is possible.
+     * We control the update loop using the variable `possibleToUpdate`.
+     * If you find that no further update is possible,
+     * At the beginning of the loop, we set `possibleToUpdate = false`.
+     * Inside the dataflow analysis, if you find that update is still possible
+     * Set the variable `possibleToUpdate = true` to keep the data flow loop going.
+     */
+    while (possibleToUpdate) {
+      possibleToUpdate = false;
+      iterationCount++;
+      writer.updateIteration();
+      /* TODO: Add the code for solving data flow equation.
+       * Please do not delete any line of code from this function.
+       * You can add code as needed.
+       * The dataflow equations are as follows (in no particular order)
+       * in[n] = use[n] ∪ (out[n] – def[n])
+       * out[n] = ∪(s in succ) {in[s]}
+       * TODO: Consider `PHINode` specially.
+       * Carefully consider the structure of `PHI_USE`, and `PHI_IN` maps.
+       * We will provide NO further hint about how to process the PHINode.
+       * Please read through the documentation. */
+
+    }
+    /* Please do not modify the following lines. */
+    writer.printLiveIns(IN);
+    writer.printLiveOuts(OUT);
+    writer.printIterationCount(iterationCount);
+    return true;
+  }
+};
+
+char Liveness::ID = 0;
+static RegisterPass<Liveness> X("liveness", "My Liveness Set Pass");
---
+++ src/hw5-util.h
@@ -0,0 +1,149 @@
+//
+// Created by saikatc on 11/14/20.
+//
+
+#ifndef LLVM_HW5_UTIL_H
+#define LLVM_HW5_UTIL_H
+#pragma once
+
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Value.h"
+#include "tee.h"
+
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+using namespace llvm;
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+    return !std::isspace(ch);
+  }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+    return !std::isspace(ch);
+  }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+  ltrim(s);
+  rtrim(s);
+}
+
+std::string getShortValueName(Value *v) {
+  if (v->getName().str().length() > 0) {
+    return "%" + v->getName().str() + "";
+  } else if (isa<Instruction>(v)) {
+    std::string s = "";
+    raw_string_ostream *strm = new raw_string_ostream(s);
+    v->print(*strm);
+    std::string inst = strm->str();
+    size_t idx1 = inst.find("%");
+    size_t idx2 = inst.find(" ", idx1);
+    if (idx1 != std::string::npos && idx2 != std::string::npos) {
+      return inst.substr(idx1, idx2 - idx1);
+    } else {
+      return inst;
+    }
+  } else if (ConstantInt *cint = dyn_cast<ConstantInt>(v)) {
+    std::string s = "";
+    raw_string_ostream *strm = new raw_string_ostream(s);
+    cint->getValue().print(*strm, true);
+    return strm->str();
+  } else {
+    std::string s = "";
+    raw_string_ostream *strm = new raw_string_ostream(s);
+    v->print(*strm);
+    std::string inst = strm->str();
+    return inst;
+  }
+}
+
+std::string instructionToString(Instruction *inst){
+  std::string s = "";
+  raw_string_ostream *strm = new raw_string_ostream(s);
+  inst->print(*strm);
+  std::string instStr = strm->str();
+  trim(instStr);
+  return instStr;
+}
+
+class DataFlowWriter {
+private:
+  std::fstream file;
+  Tee<std::fstream &, std::ostream &> tee;
+  vector<Instruction *> allInstructions;
+  int iterationCount;
+
+  void printValues(map<Instruction*, set<Value *>>& vals, std::string name) {
+    tee << "Printing " << name << "\n";
+    tee << "================================================================\n";
+    for (Instruction *inst : allInstructions) {
+      set<Value *> valsForThisInstruction = vals[inst];
+      tee << "" << instructionToString(inst)  << "";
+      tee << " " << "\n" << "\t-> " << name << ":\t[ ";
+      for(Value* v : valsForThisInstruction) {
+        tee << getShortValueName(v) << " , ";
+      }
+      tee << "]\n";
+    }
+    tee << "================================================================\n";
+  }
+
+
+public:
+  DataFlowWriter(Function &function)
+      : file(function.getName().str() + ".txt", std::ios::out | std::ios::trunc),
+        tee(file, std::cout) {
+    for (BasicBlock &basicBlocks : function) {
+      for (Instruction &instruction : basicBlocks) {
+        Instruction *instPtr = &instruction;
+        allInstructions.emplace_back(instPtr);
+      }
+    }
+    iterationCount = 0;
+  }
+
+  vector<Instruction *>& getAllInstructions(){
+    return allInstructions;
+  }
+
+  void printDefs(map<Instruction*, set<Value *>>& def) {
+    printValues(def, "DEF");
+  }
+
+  void printUses(map<Instruction*, set<Value *>>& use) {
+    printValues(use, "USE");
+  }
+
+  void printLiveIns(map<Instruction*, set<Value *>>& ins) {
+    printValues(ins, "LIVE-IN");
+  }
+
+  void printLiveOuts(map<Instruction*, set<Value *>>& outs) {
+    printValues(outs, "LIVE-OUT");
+  }
+
+  void printIterationCount(int iteration){
+    tee << "Total Number of Iteration Needed : " << iteration << "\n";
+  }
+
+  void updateIteration() {
+    iterationCount++;
+  }
+
+};
+
+#endif // LLVM_HW5_UTIL_H
