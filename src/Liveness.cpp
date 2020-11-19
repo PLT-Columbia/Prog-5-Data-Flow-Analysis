9c4af04c075f9bc2844631a84a1e63571ddc6355
Aditya Sridhar <adilovesgh@users.noreply.github.com>
2020-11-19 02:55:31 -0500

    Update Liveness.cpp

--- src/Liveness.cpp
+++ src/Liveness.cpp
@@ -67,19 +67,17 @@ struct Liveness : public FunctionPass {
   }
 
   /**
-   * This function populates the initial values of different data flows
+   * This function populates the initial values of different data flow
    * ingredients. For every instruction, if it defines anything, `def` will
-   * contain that Value `use` will contain all the values that are used in an
-   * instruction. Initially, both `in` and `out` are populated with empty set.
+   * contain that value. `use` will contain all the values that are used in an
+   * instruction. Initially, both `in` and `out` are populated with the empty set.
    * `phiUse` and `phiIn` are two special cases.
    * Not all the values are used (and thus `in`) for a `PHINode`.
-   * `PHINode` only uses the values from the basicblock it is coming from.
-   * Thus, for every instruction, `phiUse`, and `phiIN` contains a map
-   * From the incoming `BasicBlock*` to the incoming `Value *` set.
-   * Note that, all the parameters are pointers in this function.
-   * Thus the changes we are making in this function (i.e. we are populating the
-   * values) Will be reflected to the original maps from where it is being
-   * ccalled.
+   * `PHINode` only uses the values from the predecessor basic block it is coming from.
+   * Thus, for every instruction, `phiUse` and `phiIN` contain a map from the incoming 
+   * `BasicBlock*` to the incoming `Value*` set.
+   * Note that all the function arguments are pointers in this function.
+   * Thus, any changes made in this function will be reflected in the original maps.
    */
   void populateInitialValues(
       vector<Instruction *> &allInstructions,
@@ -137,11 +135,9 @@ struct Liveness : public FunctionPass {
 
   /*
    * The following function is for generating the data flow graph.
-   * When you find a data flow edge from an
-   * Instruction *FROM to a Instruction *TO
-   * That flows the Value *VALUE, call
-   * writer.printDataFlowEdge(FROM, TO, VALUE);
-   * To print out the edge.
+   * When you find a data flow edge from an instruction *FROM to an
+   * instruction *TO that flows the value *VALUE, call
+   * "writer.printDataFlowEdge(FROM, TO, VALUE);" to print out the edge.
    */
   void
   generateDataFlowGraph(vector<Instruction *> allInstructions,
@@ -156,9 +152,9 @@ struct Liveness : public FunctionPass {
   bool runOnFunction(Function &F) override {
     DataFlowWriter writer(F);
     vector<Instruction *> allInstructions = writer.getAllInstructions();
-    /* The Flow graph is represented as adjacency list.
-     * For every Instruction in the graph, controlFlowGraph contains a vector
-     * Of its neighboring Instruction (only outgoing edges). */
+    /* The flow graph is represented as adjacency list.
+     * For every instruction in the graph, controlFlowGraph contains a vector
+     * of its neighboring instruction (only outgoing edges). */
     map<Instruction *, vector<Instruction *>> controlFlowGraph =
         extractControlFlowGraph(F);
 
@@ -172,43 +168,43 @@ struct Liveness : public FunctionPass {
         allInstructions, DEF, USE, LIVE_IN, LIVE_OUT, PHI_USE, PHI_IN);
 
     /* These two printers show the DEFs and USEs of every instruction.
-     * You can use these two function to see the values of DEF and USE.
-     * If you do, make sure to comment out these 2 lines before submission.*/
+     * You can use these two functions to see the values of DEF and USE.
+     * If you do so, make sure to comment out these two lines before submission.*/
     // writer.printDefs(DEF);
     // writer.printUses(USE);
 
-    /* Task 1 - Finding the Data Flow Graph */
+    /* Task 1 - Generating the data flow graph */
     generateDataFlowGraph(allInstructions, DEF, USE, PHI_USE, writer);
 
-    /* Task 2 - Identifying the Live-in and Live-out variables for every
-     * instruction */
+    /* Task 2 - Identifying the live-in and live-out sets */
     int iterationCount = 0;
     bool possibleToUpdate = true;
-    /* If you change anything in the existing code, and the output format
-     * Does not match, your assignment might be penalized.*/
+    /* If you change anything in the existing code and the output format
+     * does not match, your assignment might be penalized. */
 
     /*
-     * Initially, we assume that dataflow update is possible.
+     * Initially, we assume that the data flow update is possible.
      * We control the update loop using the variable `possibleToUpdate`.
-     * Inside the dataflow analysis, when you want to terminate the updating 
-     * The LIVE_IN and LIVE_OUT sets, `possibleToUpdate = false`.
+     * Inside the data flow analysis, when you want to terminate updating 
+     * the LIVE_IN and LIVE_OUT sets, set `possibleToUpdate = false`.
      */
     while (possibleToUpdate) {
       iterationCount++;
       writer.updateIteration();
-      /* TODO: Add the code for solving data flow equation.
+      /* TODO: Add the code for solving the data flow equations.
        * Please do not delete any line of code from this function.
-       * You can add code as needed.
-       * The dataflow equations are as follows (in no particular order)
-       * in[n] = use[n] ∪ (out[n] – def[n])
-       * out[n] = ∪(s in succ) {in[s]}
-       * TODO: Consider `PHINode` specially.
+       * You can add code as necessary.
+       * The data flow equations are as follows (in no particular order):
+       *    in[n] = use[n] ∪ (out[n] – def[n])
+       *    out[n] = ∪(s in succ) {in[s]}
+       * TODO: Consider `PHINode` as a special case.
        * Carefully consider the structure of `PHI_USE`, and `PHI_IN` maps.
        * We will provide NO further hint about how to process the PHINode.
        * Please read through the documentation. */
 
       possibleToUpdate = false;
     }
+    
     /* Please do not modify the following lines. */
     writer.printLiveIns(LIVE_IN);
     writer.printLiveOuts(LIVE_OUT);
